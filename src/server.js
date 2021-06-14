f8adc22804b67c16d942d828e727a6620aec36e6
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-06-14 19:24:24 -0300

    created routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const server = express()
+const routes = require("./routes")
+
+// enable static files
+server.use(express.static("public"))
 
 // request, response
 server.get('/', (request, response) => {
-    return response.send('OIE!!!')
+    return response.sendFile(__dirname + "/views/index.html")
 })
+
+// routes
+server.use(routes)
 
 server.listen(3000, () => console.log('rodando'))
