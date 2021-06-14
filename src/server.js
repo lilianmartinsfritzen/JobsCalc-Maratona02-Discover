894a5e6527dc68150e9792251fb6589f20254d7e
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-06-14 00:19:18 -0300

    request-response

--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
 const express = require("express")
 const server = express()
 
+// request, response
+server.get('/', (request, response) => {
+    return response.send('OIE!!!')
+})
+
 server.listen(3000, () => console.log('rodando'))
