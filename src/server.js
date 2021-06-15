48697d7fa1e45e45790870fd6e311db48fa170a2
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-06-14 22:41:34 -0300

    changed profile

--- src/server.js
+++ src/server.js
@@ -2,13 +2,11 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
+// using template engine
+server.set('view engine', 'ejs')
+
 // enable static files
 server.use(express.static("public"))
-
-// request, response
-server.get('/', (request, response) => {
-    return response.sendFile(__dirname + "/views/index.html")
-})
 
 // routes
 server.use(routes)
