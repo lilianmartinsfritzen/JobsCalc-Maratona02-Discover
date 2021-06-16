9cc209d12c1970c26ec94956173637cf3a3f47ce
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-06-16 16:16:19 -0300

    mvc model applied

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const server = express()
 const routes = require("./routes")
+const path = require("path")
 
 // using template engine
 server.set('view engine', 'ejs')
+
+// change views folder location 
+server.set('views', path.join(__dirname, 'views'))
 
 // enable static files
 server.use(express.static("public"))
